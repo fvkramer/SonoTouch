316629eacd9286bfb00ea75a236b61d84b12f6fe
FilippKramer <krame490@gmail.com>
2019-01-11 22:21:45 -0800

    Fix svg bug

--- src/boombox.js
+++ src/boombox.js
@@ -19,27 +19,34 @@ const createBoomBox = (frequencyData) => {
     .domain([0, d3.max(frequencyData)])
     .range([0, 360]);
 
-  // console.log(Array.from(frequencyData));
-  console.log(frequencyData);
 
-  // const newData = [];
-
-  // for (let i = 0; i < 2; i++) {
-  //   newData.push(Math.random() * 200);
-  // }
-  svg.selectAll('circle')
-    .data(frequencyData)
-    .enter()
+  const circles = svg.selectAll('circle')
+    .data(frequencyData);
+  circles.exit().remove();
+  circles.enter()
     .append('circle')
-    .attr('r', d => rangeScale(d))
     .attr('cx', svgWidth / 2)
     .attr('cy', svgHeight / 2)
     .attr('fill', 'none')
     .attr('stroke-width', 4)
     .attr('stroke-opacity', 1)
-    .attr('stroke', d => d3.hsl(hslScale(d), 1, 0.5))
-    .exit()
-    .remove();
+    .merge(circles)
+    .attr('r', d => rangeScale(d))
+    .attr('stroke', d => d3.hsl(hslScale(d), 1, 0.5));
+  // svg.selectAll('circle')
+  //   .data(frequencyData)
+  //   .enter()
+  //   .append('circle')
+  //   .attr('r', d => rangeScale(d))
+  //   .attr('cx', svgWidth / 2)
+  //   .attr('cy', svgHeight / 2)
+  //   .attr('fill', 'none')
+  //   .attr('stroke-width', 4)
+  //   .attr('stroke-opacity', 1)
+  //   .attr('stroke', d => d3.hsl(hslScale(d), 1, 0.5))
+  //   .exit()
+  //   .remove();
 };
+
 
 export default createBoomBox;
