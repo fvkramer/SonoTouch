7f097b994a4b3d247a9c8418d289c66e06750ece
FilippKramer <krame490@gmail.com>
2019-01-14 11:09:22 -0800

    Refactor boombox script

--- src/boombox.js
+++ src/boombox.js
@@ -1,6 +1,8 @@
 const svgHeight = 300;
 const svgWidth = 450;
 
+
+const monitorWidth = 735;
 // two div elements
 
 // const boomBoxes = d3.selectAll('.boom-box');
@@ -11,12 +13,11 @@ const svg = boomBox.append('svg')
   .attr('height', svgHeight)
   .attr('width', svgWidth);
 
-
 const svgMonitor = monitor.append('svg')
   .attr('height', 400)
   .attr('width', 600);
 
-const createBoomBox = (frequencyData) => {
+const makeBoomBox = (frequencyData) => {
   const rangeScale = d3.scaleLinear()
     .domain([0, d3.max(frequencyData)])
     .range([0, svgHeight - 100]);
@@ -38,33 +39,39 @@ const createBoomBox = (frequencyData) => {
     .merge(circles)
     .attr('r', d => rangeScale(d))
     .attr('stroke', d => d3.hsl(hslScale(d), 1, 0.85));
+};
+
+
+const makeWaveForm = (frequencyData) => {
+  const rangeScale = d3.scaleLinear()
+    .domain([0, d3.max(frequencyData)])
+    .range([0, svgHeight - 100]);
+
+  const waveLength = d3.scaleLinear()
+    .domain([0, d3.max(frequencyData)])
+    .range([0, monitorWidth]);
+
+  const hslScale = d3.scaleLinear()
+    .domain([0, d3.max(frequencyData)])
+    .range([0, 360]);
 
   const rects = svgMonitor.selectAll('rect')
     .data(frequencyData);
-  rects.exit().remove();
+  rects.remove();
   rects.enter()
     .append('rect')
-    .attr('x', 50)
-    .attr('y', 50)
-    .attr('width', 100)
-    .attr('height', 50)
-    .attr('fill', 'green')
-    .attr('transform', 'translate(150, 0)rotate(90)');
-
-  // svg.selectAll('circle')
-  //   .data(frequencyData)
-  //   .enter()
-  //   .append('circle')
-  //   .attr('r', d => rangeScale(d))
-  //   .attr('cx', svgWidth / 2)
-  //   .attr('cy', svgHeight / 2)
-  //   .attr('fill', 'none')
-  //   .attr('stroke-width', 4)
-  //   .attr('stroke-opacity', 1)
-  //   .attr('stroke', d => d3.hsl(hslScale(d), 1, 0.5))
-  //   .exit()
-  //   .remove();
+    .attr('x', 175)
+    .attr('y', d => -waveLength(d))
+    .attr('width', d => rangeScale(d))
+    .attr('height', 3)
+    .attr('fill', d => d3.hsl(hslScale(d), 1, 0.85))
+    .attr('transform', 'rotate(90)');
 };
 
+const createBoomBox = (frequencyData) => {
+  makeBoomBox(frequencyData);
+
+  makeWaveForm(frequencyData);
+};
 
 export default createBoomBox;
